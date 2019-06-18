3dfc6c0caf6f80c866291804256d033e886e9a5a
Joe Kinney <joekinney53@gmail.com>
2019-06-18 00:00:15 -0500

    further added onto skeleton and finished switch functions, still need to put my data into function skeletons i have set up

--- liri.js
+++ liri.js
@@ -6,6 +6,143 @@ var Spotify = require ('node-spotify-api');
 var axios = require ('axios');
 var moment = require ('moment');
 
-var fs = require('fs');
+var fs = require ('fs');
+
+
 
 var spotify = new Spotify(keys.spotify);
+
+
+
+
+var nodeArgs = process.argv;
+var command = process.argv[2];
+var MovieSongBand = "";
+for (var i=3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+        MovieSongBand = MovieSongBand + "+" + nodeArgs[i];
+    }else {
+        MovieSongBand += nodeArgs[i];
+    }
+}
+
+console.log(command);
+console.log(process.argv);
+askLiri();
+
+
+askLiri = function () {
+
+switch (command) {
+    case ('concert-this'):
+    getConcert();
+    break;
+
+    case ('spotify-this-song'):
+        if (nodeArgs[i]) {
+            spotifySong(nodeArgs[i]);
+         } else {
+                spotifySong("The Sign");   
+            }
+        break;
+    
+    case ('movie-this'):
+         if (nodeArgs[i]) {
+             getMovie(nodeArgs[i]);
+         }  else {
+             getMovie("Mr. Nobody");
+         } 
+        break;
+   
+   
+    case ('do-what-it-says'):
+    liriSays();
+    break;          
+};
+}
+
+
+
+
+
+getMovie = function (movieName) {
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response, body) {
+
+    var data = JSON.parse(body); 
+
+    for(i=0; i < data.length; i++)
+    console.log("Release Year: " + response.data.Year);
+  })
+  .catch(function(error) {
+    if (error.response) {
+      
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+      
+      console.log(error.request);
+    } else {
+      
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  
+
+});
+}
+
+spotifySong = function (song) {
+
+}
+
+
+
+getConcert = function (artist) {
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    
+   
+    console.log(queryUrl);
+    
+    axios.get(queryUrl).then(
+      function(response) {
+        console.log("Release Year: " + response.data.Year);
+      })
+      .catch(function(error) {
+        if (error.response) {
+          
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+         
+          console.log(error.request);
+        } else {
+          
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      
+    
+    });
+};
+
+liriSays = function () {
+
+}
+
+
